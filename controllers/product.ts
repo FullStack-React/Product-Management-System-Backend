2a47382a6f037aa414501c0b63ef16a95a2077f4
Wenxuan Wu <wwx970624771@gmail.com>
2024-03-25 21:19:25 -0700

    feat: get product list & get one product api done

--- controllers/product.ts
+++ controllers/product.ts
@@ -25,7 +25,32 @@ export const createProduct = async (req: Request, res: Response) => {
   }
 };
 
-// update product by id;
-// get products list;
 // get product by id;
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ success: false, message: 'Product not found' });
+    }
+    res.status(200).json({ success: true, product });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+};
+
+// get products list;
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, products });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+};
+
+// update product by id;
 // get all products owned by a specific vendor;
--- routers/product.ts
+++ routers/product.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import { createProduct } from '../controllers/product.ts';
+import {
+  createProduct,
+  getProduct,
+  getProducts,
+} from '../controllers/product.ts';
 import { vendorAuth } from '../middlewares/auth.ts';
 
 const router = express.Router();
 
-// router.post('/create_product', createProduct);
 router.post('/create_product', vendorAuth, createProduct);
-router.post;
-
+router.get('/get_product/:id', getProduct);
+router.get('/get_products', getProducts);
 export default router;
